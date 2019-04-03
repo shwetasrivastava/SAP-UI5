a18f5bc701f3dc8c7da52e11b5669871d3e0c3e5
shwetasrivastava <shweta.srivastava010@gmail.com>
2019-04-03 14:18:39 +0530

    step-17

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,6 +2,7 @@ sap.ui.define(
   ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
   function(Controller, MessageToast, Fragment) {
     "use strict";
+
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
       onShowHello: function() {
         // read msg from i18n model
@@ -12,6 +13,7 @@ sap.ui.define(
           .getModel()
           .getProperty("/recipient/name");
         var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
         // show message
         MessageToast.show(sMsg);
       },
@@ -24,7 +26,8 @@ sap.ui.define(
           // load asynchronous XML fragment
           Fragment.load({
             id: oView.getId(),
-            name: "sap.ui.demo.walkthrough.view.HelloDialog"
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+            controller: this
           }).then(function(oDialog) {
             // connect dialog to the root view of this component (models, lifecycle)
             oView.addDependent(oDialog);
@@ -33,6 +36,10 @@ sap.ui.define(
         } else {
           this.byId("helloDialog").open();
         }
+      },
+
+      onCloseDialog: function() {
+        this.byId("helloDialog").close();
       }
     });
   }
